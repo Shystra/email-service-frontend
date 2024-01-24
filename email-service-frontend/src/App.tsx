84596308f5423182b0c65870a96963908f9f4b48
Shystra <jlsp.2019@hotmail.com>
2024-01-23 22:46:49 -0300

    feate: creating new function for to render first payment

--- email-service-frontend/src/App.tsx
+++ email-service-frontend/src/App.tsx
@@ -1,14 +1,61 @@
 
 
+import { ChangeEvent, useState } from 'react'
 import './App.css'
+import PrimaryInput from './components/Input/PrimaryInput'
+import { Button, Spacer } from '@chakra-ui/react';
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState("");
 
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value)
+  }
 
   return (
-    <>
-      
-    </>
+    <div className='container'>
+      <form>
+        <div className='name-form-container'>
+
+          <PrimaryInput
+            value={email}
+            onChange={handleChange}
+            name="firstName"
+            label="Nome"
+            placeholder='Lucas'
+
+          />
+          <PrimaryInput
+            value={firstName}
+            onChange={handleChange}
+            name="secondName"
+            label="Sobrenome"
+            placeholder='Souza'
+
+          />
+        </div>
+        <Spacer height="4"/>
+
+
+        <PrimaryInput
+          value={secondName}
+          onChange={handleChange}
+          name="email"
+          label="Digite o seu e-mail"
+          placeholder='suporte@exemplo.com'
+
+        />
+        <Spacer height="6"/>
+        <Button colorScheme='green' width="100%" >Enviar</Button>
+      </form>
+
+      <div className='product-details'>
+        <p>Você irá pagar</p>
+        <span>R$ 250,00</span>
+      </div>
+    </div>
   )
 }
 
--- email-service-frontend/src/components/Input/PrimaryInput.tsx
+++ email-service-frontend/src/components/Input/PrimaryInput.tsx
@@ -1,11 +1,32 @@
-interface PrimaryInputProps {
+import { Input } from "@chakra-ui/react";
+import { ChangeEventHandler } from "react";
+import './Primary-Input.css';
 
+interface PrimaryInputProps {
+    name: string,
+    value: string,
+    onChange: ChangeEventHandler<HTMLInputElement>
+    label: string,
+    placeholder: string
 }
-export default function PrimaryInput({}: PrimaryInputProps){
-    return(
-        <>
-        
-        
-        </>
+export default function PrimaryInput({ name, value, onChange, label, placeholder }: PrimaryInputProps) {
+    return (
+
+
+        <div className="input-container">
+            <label className="label">{label}</label>
+            <Input
+                variant='filled'
+                placeholder={placeholder}
+                name={name}
+                value={value}
+                onChange={onChange}
+            />
+            
+
+        </div>
+
+
+
     )
 }
